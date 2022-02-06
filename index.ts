7a30fcb84acd60500e6d210655d19e54cccd9984
reanimation47 <reanimation47@gmail.com>
2022-02-06 21:41:22 +0700

    sth

--- index.ts
+++ index.ts
@@ -65,7 +65,7 @@ client.on('ready', () => {
 })
 //Jabba's gifs
 const laugh = ["https://tenor.com/view/jabba-the-hutt-star-wars-gif-12663815", "https://tenor.com/view/jabba-laugh-star-wars-haha-mood-gif-11764102", "https://tenor.com/view/woahaha-jabba-starwars-gif-19246551", "https://tenor.com/view/jabba-the-hutt-laugh-monster-gif-13644611", "https://tenor.com/view/jabba-chumbolo-laugh-botek-solo-gif-24199365"];
-
+const sexi = ["https://tenor.com/view/leia-jabba-gif-8984830","https://tenor.com/view/leia-organa-slave-leia-leia-bikini-leia-metal-bikini-star-wars-gif-17965323","https://tenor.com/view/jabba-leia-jabba-leia-jabba-lick-gif-18933413","https://tenor.com/view/slave-leia-jabba-jabba-leia-jabba-lick-bricosplay-gif-23134280"]
 
 
 //Random imgur
@@ -253,6 +253,7 @@ client.on('messageCreate', (message) => {
         if (x > 10) {
             x = 10
         }
+        
 
 
         message.channel.send(`jabba chửi bảnh ${x} lần`);
