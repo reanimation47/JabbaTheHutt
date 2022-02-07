b72328039f2287268c964e87e52b531940df61b6
reanimation47 <reanimation47@gmail.com>
2022-02-07 19:03:45 +0700

    6 to 5

--- index.ts
+++ index.ts
@@ -73,7 +73,7 @@ const sexi = ["https://tenor.com/view/leia-jabba-gif-8984830", "https://tenor.co
 //Random imgur
 function randomString() {
   var chars = '01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghiklmnopqrstuvwxyz';
-  var stringlength = 6; /* could be 6 or 7, but takes forever because there are lots of dead images */
+  var stringlength = 5; /* could be 6 or 7, but takes forever because there are lots of dead images */
   var text = '';
   for (var i = 0; i < stringlength; i++) {
     var rnum = Math.floor(Math.random() * chars.length);
