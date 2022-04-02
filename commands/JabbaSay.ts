e2ccea1163276ff3162005974115fad602edf179
reanimation47 <reanimation47@gmail.com>
2022-04-02 21:53:11 +0700

    added deez

--- commands/JabbaSay.ts
+++ commands/JabbaSay.ts
@@ -6,7 +6,7 @@ import { client } from "../index"
 
 module.exports = {
     name: 'JabbaSay',
-    description:'Jabba repeat something',
+    description:'Jabba repeat something, and say deeznut..',
     async execute(message) {
         let mes: string = message.content.toLowerCase()
         let mess: string = removeVN(mes)
@@ -14,6 +14,11 @@ module.exports = {
         if (mess.includes('jabba say')) {
             mess = mess.replace('jabba say','')
             client.channels.cache.get("794818082584526868").send(mess);
+        }
+        if (mess.includes('699533303044374528')) {
+            // let user = client.users.cache.get("699533303044374528");
+            // console.log(user)
+            message.reply('deeznuts'+getRandom(randomEmotes))
         }
     }
 }
