9ca0d0b29a620b0a1536faa1a258d54614f4144c
Danny Le <danny.le@playstudios.asia>
2023-07-06 19:12:03 +0700

    nothing

--- commands/OpenAI.ts
+++ commands/OpenAI.ts
@@ -27,7 +27,7 @@ module.exports = {
             let _reply = await ask_gpt(_template_msg);
             message.channel.send(_reply);
         }else if (mess.includes('jpt')){
-            let _random:any = Math.random();
+            let _random:number = Math.random();
             let _template_msg:string;
             mess = mess.replace('jpt','')
             _template_msg = `I want you to act as a drunk person. You will only answer like a very drunk person texting and nothing else. Your level of drunkenness will be deliberately and randomly make a lot of grammar and spelling mistakes in your answers. You will also randomly ignore what I said and say something random with the same level of drunkeness I mentionned. Do not write explanations on replies. My sentence is ${mess}`
